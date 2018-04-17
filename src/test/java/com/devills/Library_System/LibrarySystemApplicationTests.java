0faccc1b263f9ee45187a4d5e12e2a5570d51c82
frank mokhabuki <frank.mokhabuki@thumbtribe.mobi>
2018-04-17 20:42:00 +0200

    added initial files

---
+++ src/test/java/com/devills/Library_System/LibrarySystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devills.Library_System;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibrarySystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
